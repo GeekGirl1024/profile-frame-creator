899354b342f04c9b47982e85111c0ed5076fa1f4
Sophia Lee <sophia@transgirltheory.com>
2022-08-08 19:52:40 -0500

    add background and support for non square pictures

--- src/profileFrameCreator/ProfileFrameCreator.tsx
+++ src/profileFrameCreator/ProfileFrameCreator.tsx
@@ -14,13 +14,18 @@ import './ProfileFrameCreator.css'
 class ProfileFrameCreator extends React.Component {
 
   private profileImage = new Image();
+  private backgroundImage = new Image();
   private canvasElement: HTMLCanvasElement | undefined;
   private canvasContext: CanvasRenderingContext2D | undefined;
   private fileReader = new FileReader();
-
   private filesElement: HTMLInputElement | undefined;
-
   private downloadLinkElement: HTMLAnchorElement | undefined;
+
+  private frames: string[] = [
+    BasicUkraineImage,
+    CircleUkraineImage,
+    LeftBottomCircleUkraine,
+    RightBottomFrameUkraine];
   
 
   /**
@@ -34,12 +39,17 @@ class ProfileFrameCreator extends React.Component {
     this.canvasElement = document.getElementById('mycanvas') as HTMLCanvasElement;
     this.canvasContext = this.canvasElement.getContext('2d') as CanvasRenderingContext2D;
     this.profileImage.addEventListener("load", ((event: Event) => {
-      this.canvasContext?.drawImage(this.profileImage, 0, 0, 512, 512);
+      this.SetProfileImage();
+      
     }).bind(this));
+
+    this.backgroundImage.style.filter="blur(5px)";
 
     this.fileReader.addEventListener("load", ((event: ProgressEvent<FileReader>) => {
       if (event.target && event.target.result){
+        this.backgroundImage.src = event.target.result as string;
         this.profileImage.src = event.target.result as string;
+        
       }
     }).bind(this));
 
@@ -47,6 +57,52 @@ class ProfileFrameCreator extends React.Component {
 
     this.downloadLinkElement = document.createElement('a');
     this.downloadLinkElement.download = "my-image.png";
+  }
+
+  SetProfileImage(){
+    if (!this.canvasContext){
+      return;
+    }
+    let width = 512;
+    let height = 512;
+    let offsetx = 0;
+    let offsety = 0;
+    this.canvasContext.clearRect(0,0,512,512);
+    this.canvasContext.fillStyle="#555555";
+    this.canvasContext.fillRect(0,0,512,512);
+    if (this.profileImage.height == this.profileImage.width){
+    } else if (this.profileImage.height > this.profileImage.width) {
+      width = this.profileImage.width / this.profileImage.height * 512;
+      offsetx = (512 - width)/2;
+
+      /*
+      this.canvasContext.drawImage(this.backgroundImage, (512-width*3)/2, (512-height*3), width*3, height*3);
+      
+      this.canvasContext.save();
+      this.canvasContext.filter = "blur(10px)";
+      let backgroundDataUrl = this.canvasElement?.toDataURL();
+      
+      this.canvasContext.restore();
+      //this.canvasContext.putImageData(backgroundDataUrl, 0, 0);
+      */
+      
+    } else {
+      height = this.profileImage.height / this.profileImage.width * 512;
+      offsety = (512 - height)/2;
+
+      /*
+      this.canvasContext.drawImage(this.backgroundImage, (512-width*3)/2, (512-height*3), width*3, height*3);
+      
+      this.canvasContext.save();
+      this.canvasContext.filter = "blur(10px)";
+      let backgroundDataUrl = this.canvasContext.getImageData(0,0,512,512);
+      this.canvasContext.restore();
+      this.canvasContext.putImageData(backgroundDataUrl, 0, 0);
+
+      */
+    }
+    
+    this.canvasContext.drawImage(this.profileImage, offsetx, offsety, width, height);
   }
 
   DownloadImage(){
@@ -67,7 +123,7 @@ class ProfileFrameCreator extends React.Component {
 
   SetFrame(htmlImageElement: HTMLImageElement) {
     if (this.profileImage.src) {
-      this.canvasContext?.drawImage(this.profileImage, 0, 0, 512, 512);
+      this.SetProfileImage();
       this.canvasContext?.drawImage(htmlImageElement, 0, 0, 512, 512);
     }
   }
@@ -81,10 +137,21 @@ class ProfileFrameCreator extends React.Component {
           <h1>Profile Frame Creator</h1>
           <Box direction="row" >
             <Box direction='column' >
-              <GrommetImage className="profile-frame" width="100" height="100" src={BasicUkraineImage} onClick={(element) => { this.SetFrame(element.target as HTMLImageElement); }}></GrommetImage>
-              <GrommetImage className="profile-frame" width="100" height="100" src={CircleUkraineImage} onClick={(element) => { this.SetFrame(element.target as HTMLImageElement); }}></GrommetImage>
-              <GrommetImage className="profile-frame" width="100" height="100" src={LeftBottomCircleUkraine} onClick={(element) => { this.SetFrame(element.target as HTMLImageElement); }}></GrommetImage>
-              <GrommetImage className="profile-frame" width="100" height="100" src={RightBottomFrameUkraine} onClick={(element) => { this.SetFrame(element.target as HTMLImageElement); }}></GrommetImage>
+              {
+                this.frames.map(
+                  (src:string) => {
+                    return <GrommetImage
+                        key={src}
+                        className="profile-frame"
+                        width="100"
+                        height="100"
+                        src={src}
+                        onClick={(element) => { this.SetFrame(element.target as HTMLImageElement); }}
+                        >
+                      </GrommetImage>
+                  }
+                )
+              }
             </Box>
             <Box direction='column'>
               <Box direction='row'>
